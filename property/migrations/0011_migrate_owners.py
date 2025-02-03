23eb7912ecf4a4ca72e03025a3815de3646828d7
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-02-03 19:15:15 +0500

    Update 0011_migrate_owners.py

--- property/migrations/0011_migrate_owners.py
+++ property/migrations/0011_migrate_owners.py
@@ -1,32 +1,19 @@
 from django.db import migrations
-from django.db.models import F
 
 
 def migrate_owners(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
-    FlatOwner = Flat.owners.through  
 
-    owner_data = {}
-    for flat in Flat.objects.all().iterator(chunk_size=1000):  
-        if flat.owner:
-            for owner_name in flat.owner.split(','):
-                owner_name = owner_name.strip()
-                if owner_name not in owner_data:
-                    owner_data[owner_name] = []
-                owner_data[owner_name].append(flat.id)
-
-    owners = [Owner(full_name=name) for name in owner_data.keys()]
-    Owner.objects.bulk_create(owners, ignore_conflicts=True)  
-
-    owner_name_to_id = {owner.full_name: owner.id for owner in Owner.objects.filter(full_name__in=owner_data.keys())}
-
-    flat_owners = []
-    for owner_name, flat_ids in owner_data.items():
-        owner_id = owner_name_to_id[owner_name]
-        flat_owners.extend([FlatOwner(flat_id=flat_id, owner_id=owner_id) for flat_id in flat_ids])
-
-    FlatOwner.objects.bulk_create(flat_owners)
+    for flat in Flat.objects.all().iterator():
+        owner, created = Owner.objects.get_or_create(
+            full_name=flat.owner,
+            defaults={
+                'phone_number': flat.owners_phonenumber,
+                'normalized_phone': flat.owner_pure_phone,
+            }
+        )
+        owner.flats.set([flat])
 
 
 class Migration(migrations.Migration):
